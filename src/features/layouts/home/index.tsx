c8631c78899d9650912377de02ed7a8849c17d43
yura <yurii@niuitmo.ru>
2024-06-14 22:28:21 +0300

    first demo-version with about

--- src/features/layouts/home/index.tsx
+++ src/features/layouts/home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Header } from "../../../entities/Header";
-import { Options } from "../../../entities/Options";
+import { Header } from "@/entities/Header";
+import { Options } from "@/entities/Options";
 
 
 export const HomePage: React.FC = () => {
