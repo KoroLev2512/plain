89a81d0180e9212e457f51366881c3fb75042601
yura <yurii@niuitmo.ru>
2024-06-14 22:03:26 +0300

    first demo-version with about

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -1,21 +1,20 @@
-import classNames from 'classnames';
 import React from "react";
 
 import styles from "./styles.module.scss";
 
 export const Text = <C extends React.ElementType>({
-	as,
-	children,
-	className,
-}: {
-  as?: C;
-  children: React.ReactNode;
-  className?: string;
+													  as,
+													  children,
+													  className,
+												  }: {
+	as?: C;
+	children: React.ReactNode;
+	className?: string;
 }) => {
 	const Component = as || "p";
 
 	return (
-		<Component className={classNames(styles.text, className)}>
+		<Component className={`${styles.text} ${className ? className : ''}`}>
 			{children}
 		</Component>
 	);
