cc17a9568256ef6e932323905152692f7f42b90d
yura <yurii@niuitmo.ru>
2024-07-05 19:50:42 +0300

    fix

--- src/features/wrappers/AppWrapper.tsx
+++ src/features/wrappers/AppWrapper.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Layout } from "./types";
 import {ContentWrapper} from "@/features/wrappers/ContentWrapper";
 import {Header} from "@/entities/Header";
-import {useTranslation} from "react-i18next";
+// import {useTranslation} from "react-i18next";
 
 import styles from "./styles.module.scss";
 
 const AppWrapper = (props: Layout) => {
 	const {children} = props;
-	const {t, i18n} = useTranslation();
-	const changeLanguage = (language: string) => {
-		i18n.changeLanguage(language);
-	};
+	// const {t, i18n} = useTranslation();
+	// const changeLanguage = (language: string) => {
+	// 	i18n.changeLanguage(language);
+	// };
 
 	return (
 		<div className={styles.appWrapper}>
@@ -22,4 +22,3 @@ const AppWrapper = (props: Layout) => {
 };
 
 export default AppWrapper;
-
