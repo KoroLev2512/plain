4953fcb0beb555e11c73b567db905351281920c3
ruddnev <war.rudnevn@gmail.com>
2024-07-12 20:36:50 +0300

    make locations preferences work

--- src/shared/ui/Filter/Filter.tsx
+++ src/shared/ui/Filter/Filter.tsx
@@ -3,19 +3,29 @@ import React from 'react';
 import styles from "./styles.module.scss"
 
 export const Filter = () => {
+
+    const filters = ["mainstream", "balanced", "hidden gems"]
+    const [selectedFilters, setSelectedFilters] = React.useState<string[]>([]);
+
+    const handleFilterClick = (filter: string) => {
+        if (selectedFilters.includes(filter)) {
+            setSelectedFilters(selectedFilters.filter(f => f !== filter));
+        } else {
+            setSelectedFilters([...selectedFilters, filter]);
+        }
+    }
+
     return (
         <div className={styles.wrapper}>
             {/* uncomment line below to check for selected style */}
             {/* <div className={styles.filter + " " + styles.selected}> */}
-            <div className={styles.filter}>
-                mainstream
-            </div>
-            <div className={styles.filter}>
-                balanced
-            </div>
-            <div className={styles.filter}>
-                hidden gems
-            </div>
+            {filters.map((filter, index) => (
+                <div key={index}
+                     className={`${styles.filter} ${selectedFilters.includes(filter) ? styles.selected : ""}`}
+                     onClick={(e) => {handleFilterClick(filter)}}>
+                    {filter}
+                </div>
+            ))}
         </div>
     );
 };
