0cf93e51ba5e238fc50209e3a331553b982de35d
ruddnev <war.rudnevn@gmail.com>
2024-07-21 04:57:50 +0300

    languages v1

--- src/entities/LanguageFlag/index.tsx
+++ src/entities/LanguageFlag/index.tsx
@@ -39,83 +39,16 @@ export default function LanguageFlag() {
         setAnchorEl(null);
     };
 
-
     return (
         <div className={styles.flags}>
-            {/*<Tooltip title={t('language')} arrow>*/}
-                <div className={styles.text} onClick={handleClick}>
-                    language
-                </div>
-            {/*</Tooltip>*/}
-            <Menu
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                onClick={handleClose}
-                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                disableScrollLock={true}
-                PaperProps={{
-                    elevation: 0,
-                    sx: {
-                        overflow: 'visible',
-                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                        mt: 1.5,
-                        bgcolor: '#F8F8F8',
-                        '& .MuiAvatar-root': {
-                            width: 32,
-                            height: 32,
-                            ml: -0.5,
-                            mr: 1,
-                        },
-                        '&:before': {
-                            content: '""',
-                            display: 'block',
-                            position: 'absolute',
-                            top: 0,
-                            right: 14,
-                            width: 10,
-                            height: 10,
-                            bgcolor: '#F8F8F8',
-                            transform: 'translateY(-50%) rotate(45deg)',
-                            zIndex: 0,
-
-                        },
-                    },
-                }}
-            >
-                {languages.map(({ code, name, country_code, icon}) => (
-                    <Tooltip title={name} arrow placement='left'>
-                        <MenuItem key={code} style={{
-                            backgroundColor: currentLanguageCode === code ? '#D8D8D8' : '#F8F8F8',
-                            border: currentLanguageCode === code ? '1px solid #989898' : '#F8F8F8'
-                        }}>
-                            <IconButton
-                                className={styles.flags}
-                                onClick={() => {
-                                    i18next.changeLanguage(code)
-                                }}
-                            >
-                                <img
-                                    src={icon}
-                                    alt={name}
-                                    width={40}
-                                    height={40}
-                                />
-                                <div className={`flag-icon flag-icon-${country_code}`}
-                                     style={{
-                                         opacity: currentLanguageCode === code ? 1 : 0.3,
-                                         width: '1rem',
-                                         height: '1rem',
-                                     }}
-                                >
-                                </div>
-                            </IconButton>
-                        </MenuItem>
-                    </Tooltip>
+            <div className={styles.text} onClick={handleClick}>
+                <p>language</p>
+            </div>
+            <div className={styles.languages}>
+                {languages.map(({ code, name, country_code, icon }) => (
+                    <p key={code} onClick={() => i18next.changeLanguage(code)} className={currentLanguageCode == code ? styles.active : ""}>{country_code}</p>
                 ))}
-            </Menu>
-
+            </div>
         </div>
     )
 }
